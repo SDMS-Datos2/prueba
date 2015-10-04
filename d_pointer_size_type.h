9635bcc6a75a023e58446b07398feb4c1a7b7c9b
nitsuj07 <justinjosue01@gmail.com>
2015-10-04 14:39:44 -0600

    Update d_pointer_size_type.h

--- d_pointer_size_type.h
+++ d_pointer_size_type.h
@@ -1,10 +1,39 @@
 #ifndef D_POINTER_SIZE_TYPE_H
 #define D_POINTER_SIZE_TYPE_H
+#include "dobject.h"
+#include <cstdlib>
+
 
 class d_pointer_size_type
 {
+private:
+    unsigned int ID;
+    unsigned int puntero;
+    unsigned int espacio;
+    unsigned char tipo;
+    unsigned char referencia;
 public:
     d_pointer_size_type();
+    int getID()const;
+    int getPtr()const;
+    int getEsp();
+    char getTipo();
+    char getRef();
+    void setID(int pID);
+    void setPtr(int pPtr);
+    void setEsp(int pSpace);
+    void setTipo(char pType);
+    void setRef(char pRef);
+    bool operator== (const d_pointer_size_type &pPointer);
+    void operator= (const d_pointer_size_type &pPointer);
+    void operator= (const dObject &pDObject);
+    void operator= (const int pInt);
+    //Completar
+    /**
+    dObject operator* (const int &p);
+    void operator++ (const int p);
+    void operator-- (const int p);*/
+
 };
 
-#endif // D_POINTER_SIZE_TYPE_H
+#endif // D_
